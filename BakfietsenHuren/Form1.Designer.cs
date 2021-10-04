606aa7b3f3825603bc5ebf40f30a61174471d01a
S3ntrail <46055932+S3ntrail@users.noreply.github.com>
2021-10-04 19:51:01 +0200

    Add project files.

---
+++ BakfietsenHuren/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace BakfietsenHuren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ckbBycicleOne = new System.Windows.Forms.CheckBox();
+            this.chkBycicleTwo = new System.Windows.Forms.CheckBox();
+            this.chkBycicleThree = new System.Windows.Forms.CheckBox();
+            this.numDays = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numHelmet = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chkRainroof = new System.Windows.Forms.CheckBox();
+            this.Subtotaal = new System.Windows.Forms.Label();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numDays)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHelmet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ckbBycicleOne
+            // 
+            this.ckbBycicleOne.AutoSize = true;
+            this.ckbBycicleOne.Location = new System.Drawing.Point(13, 50);
+            this.ckbBycicleOne.Margin = new System.Windows.Forms.Padding(50);
+            this.ckbBycicleOne.Name = "ckbBycicleOne";
+            this.ckbBycicleOne.Size = new System.Drawing.Size(73, 17);
+            this.ckbBycicleOne.TabIndex = 0;
+            this.ckbBycicleOne.Text = "Bakfiets 1";
+            this.ckbBycicleOne.UseVisualStyleBackColor = true;
+            // 
+            // chkBycicleTwo
+            // 
+            this.chkBycicleTwo.AutoSize = true;
+            this.chkBycicleTwo.Location = new System.Drawing.Point(13, 78);
+            this.chkBycicleTwo.Margin = new System.Windows.Forms.Padding(50);
+            this.chkBycicleTwo.Name = "chkBycicleTwo";
+            this.chkBycicleTwo.Size = new System.Drawing.Size(73, 17);
+            this.chkBycicleTwo.TabIndex = 1;
+            this.chkBycicleTwo.Text = "Bakfiets 2";
+            this.chkBycicleTwo.UseVisualStyleBackColor = true;
+            // 
+            // chkBycicleThree
+            // 
+            this.chkBycicleThree.AutoSize = true;
+            this.chkBycicleThree.Location = new System.Drawing.Point(13, 109);
+            this.chkBycicleThree.Margin = new System.Windows.Forms.Padding(50);
+            this.chkBycicleThree.Name = "chkBycicleThree";
+            this.chkBycicleThree.Size = new System.Drawing.Size(73, 17);
+            this.chkBycicleThree.TabIndex = 2;
+            this.chkBycicleThree.Text = "Bakfiets 3";
+            this.chkBycicleThree.UseVisualStyleBackColor = true;
+            // 
+            // numDays
+            // 
+            this.numDays.Location = new System.Drawing.Point(13, 179);
+            this.numDays.Name = "numDays";
+            this.numDays.Size = new System.Drawing.Size(120, 20);
+            this.numDays.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kies uw bakfiets";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kies aantal dagen";
+            // 
+            // numHelmet
+            // 
+            this.numHelmet.Location = new System.Drawing.Point(138, 252);
+            this.numHelmet.Name = "numHelmet";
+            this.numHelmet.Size = new System.Drawing.Size(120, 20);
+            this.numHelmet.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Accesoires";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Helmen";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Knie / Arm bescherming";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(138, 278);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Regendak";
+            // 
+            // chkRainroof
+            // 
+            this.chkRainroof.AutoSize = true;
+            this.chkRainroof.Location = new System.Drawing.Point(243, 304);
+            this.chkRainroof.Name = "chkRainroof";
+            this.chkRainroof.Size = new System.Drawing.Size(15, 14);
+            this.chkRainroof.TabIndex = 12;
+            this.chkRainroof.UseVisualStyleBackColor = true;
+            // 
+            // Subtotaal
+            // 
+            this.Subtotaal.AutoSize = true;
+            this.Subtotaal.Location = new System.Drawing.Point(521, 25);
+            this.Subtotaal.Name = "Subtotaal";
+            this.Subtotaal.Size = new System.Drawing.Size(52, 13);
+            this.Subtotaal.TabIndex = 13;
+            this.Subtotaal.Text = "Subtotaal";
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(589, 25);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(38, 13);
+            this.lblSubtotal.TabIndex = 14;
+            this.lblSubtotal.Text = "EURO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(589, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "BELASTING";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(521, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Btw 21%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(589, 92);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "TOTAAL";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(521, 92);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Totaal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.Subtotaal);
+            this.Controls.Add(this.chkRainroof);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numHelmet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numDays);
+            this.Controls.Add(this.chkBycicleThree);
+            this.Controls.Add(this.chkBycicleTwo);
+            this.Controls.Add(this.ckbBycicleOne);
+            this.Name = "Form1";
+            this.Text = "Bycicle Rental Application";
+            ((System.ComponentModel.ISupportInitialize)(this.numDays)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHelmet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox ckbBycicleOne;
+        private System.Windows.Forms.CheckBox chkBycicleTwo;
+        private System.Windows.Forms.CheckBox chkBycicleThree;
+        private System.Windows.Forms.NumericUpDown numDays;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numHelmet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chkRainroof;
+        private System.Windows.Forms.Label Subtotaal;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
